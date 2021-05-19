ba47ef90fc696722e67da7b8cc1cfe57acd78104
Pavel Koryakin <unclekoin@gmail.com>
2021-05-19 13:24:48 +0300

    init: start project

---
+++ README.md
@@ -0,0 +1,8 @@
+# Auth App
+
+```bash
+$ npm install -D webpack webpack-cli webpack-dev-server 
+$ npm install -D html-webpack-plugin
+$ npm install -D clean-webpack-plugin
+$ npm i -D source-map-loader
+```
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HTMLPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.[chunkhash].js',
+    path: path.resolve(__dirname, 'public')
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 4200,
+  },
+  plugins: [
+    new HTMLPlugin({
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: "pre",
+        use: ["source-map-loader"],
+      },
+    ],
+  },
+}
+
